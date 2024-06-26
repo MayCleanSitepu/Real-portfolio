a90ea6fcb9be658e94e184f82c0c10cb40168774
MayCleanSitepu <113108027+MayCleanSitepu@users.noreply.github.com>
2024-06-26 20:04:20 +0700

    new me

--- src/app/page.js
+++ src/app/page.js
@@ -33,15 +33,29 @@ export default function Home() {
   
 
   return (
-    <main class="overflow-x-hidden text-neutral-300 antialiased selection:bg-slate-300 selection:text-cyan-900">
-      <div class="absolute top-0 z-[-2] h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      <AnimatePresence mode='wait'>
-        {isLoading &&  <Preloader/>}
-      </AnimatePresence>
-      <Scene/>
-      <Hero/> 
-      <Edu/>
-      <Tech/> 
-    </main>
+    // <main class="overflow-x-hidden text-neutral-300 antialiased selection:bg-slate-300 selection:text-cyan-900">
+    //   <div class="absolute top-0 z-[-2] h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    //   <AnimatePresence mode='wait'>
+    //     {isLoading &&  <Preloader/>}
+    //   </AnimatePresence>
+    //   <Scene/>
+    //   <Hero/> 
+    //   <Edu/>
+    //   <Tech/> 
+    // </main>
+
+    <>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div>
+        <h1 className="text-white text-9xl self-center">iNi PoRtFoliO</h1>
+        <br/>
+        <br/>
+        <h2 className="text-white text-8xl self-center">May Clean</h2>
+        <br/>
+        <br/>
+        <h2 className="text-white text-7xl items-center">🙏🏻</h2>
+      </div>
+    </div>
+  </>
   )
 }
