bcc485afd7493427d5d4962fd6788f1f5fd9a1cc
MayCleanSitepu <maycleansitepu0204@mail.ugm.ac.id>
2024-07-17 20:12:18 +0700

    add card

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,68 +1,55 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from 'react'
+import Image from 'next/image'
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-
-
-// import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
-
-export default function App() {
+function projects() {
   return (
-    <>
-    <div className="flex flex-row justify-center items-center mt-40 w-[30vw] h-[50vh]">
-      <div className="flex flex-row justify-center items-center ">
-        <Swiper
-          effect={'coverflow'}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={'auto'}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          pagination={true}
-          modules={[EffectCoverflow, Pagination]}
-          className="mySwiper"
-        >
-          
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-          </SwiperSlide>
-        </Swiper>
+    <div>
+      <div id="konten" className="flex flex-col justify-center items-center mt-10">
+        <h1 className="text-[#373737] text-4xl font-semibold">Projects</h1>
       </div>
+      <div id="isi" className="bg-red-500">
+        <div className="card bg-white/50 backdrop-blur-[80%] w-96 shadow-xl">
+          <figure>
+          <Image
+          width={0}
+          height={0}
+          src="/assets/kevinRushProfile.jpg"
+          alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Shoes!
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">Fashion</div>
+              <div className="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card glass w-96">
+          <figure>
+          <Image
+                width={300}
+                height={200}
+                src="/assets/kevinRushProfile.jpg"
+                alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Learn now!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr />
     </div>
-    </>
-  );
+  )
 }
+
+export default projects
