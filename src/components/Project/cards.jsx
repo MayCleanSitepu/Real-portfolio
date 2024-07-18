12ac1db24fb7e5e2680ed541297bc0b071cc2c38
MayCleanSitepu <113108027+MayCleanSitepu@users.noreply.github.com>
2024-07-18 18:08:48 +0700

    migrating card component

---
+++ src/components/Project/cards.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import Image from "next/image";
+import { FaRegHeart } from "react-icons/fa";
+import { IoMdHeart } from "react-icons/io";
+import Img from "next/image";
+import { useState, useEffect } from "react";
+import Horizontal from "../Partial";
+
+const ProjectCard = ({ name, title, desc, date, views, imageSrc, fakeDigit, fakeCount }) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [likeCount, setIsLikeCount] = useState(fakeCount);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const handleLikeToggle = () => {
+    setIsLiked(!isLiked);
+    setIsLikeCount(isLiked ? likeCount - 1 : likeCount + 1);
+    setIsAnimating(true);
+  };
+
+  useEffect(() => {
+    if (isAnimating) {
+      const timer = setTimeout(() => {
+        setIsAnimating(false);
+      }, 500); // Durasi animasi
+      return () => clearTimeout(timer);
+    }
+  }, [isAnimating]);
+
+  return (
+    <div className="flex flex-col items-center w-full p-5 md:p-10">
+      <div className="flex flex-col w-full max-w-3xl gap-5">
+        <div className="flex flex-row gap-2 select-none">
+          <Img
+            src={"/assets/kevinRushProfile.jpg"}
+            alt="Profile"
+            width={25}
+            height={25}
+            className="rounded-full"
+            loading="lazy"
+          />
+          <h1 className="font-medium text-[#373737]">{name}</h1>
+        </div>
+        <div className="flex flex-col md:flex-row gap-5 w-full">
+          <div className="flex flex-col gap-3 w-full">
+            <h2 className="font-bold text-2xl text-[#373737]">{title}</h2>
+            <p className="text-[#373737] font-medium line-clamp-3">{desc}</p>
+            <div className="flex flex-row justify-between items-center">
+              <div className="flex flex-row gap-2">
+                <div className="flex flex-row items-center gap-2">
+                  <h1 className="font-bold text-2xl text-[#373737]">✦</h1>
+                  <h1 className="text-sm font-bold text-[#373737]">{date}</h1>
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                  <h1>🔥</h1>
+                  <h1 className="text-sm font-bold text-[#373737]">{views}</h1>
+                </div>
+              </div>
+              <div onClick={handleLikeToggle} className="flex items-center cursor-pointer">
+                {isLiked ? <IoMdHeart className=" text-[1.790rem] text-red-600" /> : <FaRegHeart className="  text-2xl text-[#373737]" />}
+                <span className="text-[#373737] select-none text-sm font-bold ms-2">{fakeDigit}</span>
+                <h1 className={`text-[#373737] select-none text-sm font-bold ${isAnimating ? 'animate-spin-grow' : ''}`}>{likeCount}</h1>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center w-full md:w-1/2">
+            <Img
+              src={imageSrc}
+              alt="Project Image"
+              width={500}
+              height={700}
+              className="select-none"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,104 +1,37 @@
-import React from "react";
-import Image from "next/image";
-import { FaRegHeart } from "react-icons/fa";
-import { IoMdHeart } from "react-icons/io";
-import Img from "next/image";
-import { useState, useEffect } from "react";
+import React from 'react'
+import ProjectCard from './cards'
 
 const Projects = () => {
-
-
-  const [isLiked, setIsLiked] = useState(false);
-  const [likeCount, setIsLikeCount] = useState(8);
-  const [isAnimating, setIsAnimating] = useState(false);
-
-  const handleLikeToggle = () => {
-    setIsLiked(!isLiked);
-    setIsLikeCount(isLiked ? likeCount - 1 : likeCount + 1);
-    setIsAnimating(true);
-  };
-
-  useEffect(() => {
-    if (isAnimating) {
-      const timer = setTimeout(() => {
-        setIsAnimating(false);
-      }, 500); // Durasi animasi
-      return () => clearTimeout(timer);
-    }
-  }, [isAnimating]);
-
   return (
-    <div>
-      <div
+    <>
+    <div
         id="konten"
         className="flex flex-col justify-center items-center mt-5"
       >
         <h1 className="text-[#373737] text-4xl font-[900]">Projects</h1>
       </div>
-      <div id="isi" className=" bg-purple-500 flex justify-center w-full p-10">
-
-        <div>
-
-          <div id="x" className=" px-12 flex flex-row gap-2">
-            <Img
-            src={"/assets/kevinRushProfile.jpg"}
-            alt="Shoes"
-            width={25}
-            height={25}
-            className="rounded-full"
-            />
-              <h1 className="font-[500] text-[#373737]">Jose Morinho</h1>
-          </div>
-          <div id="y" className=" w-[60vw] flex flex-row gap-10 justify-between px-10">
-            <div className=" w-full flex flex-col gap-1 p-2">
-                <h2 className=" font-[900] text-[#373737] text-2xl">Muhammadiyah Web Content Collaboration</h2>
-                <h1 className=" text-[#373737] line-clamp-3">Garbage collection in Python is the process of automatically identifying and reclaiming memory that is no longer in use by the program. This helps to manage memory efficiently and prevent memory leaks.</h1>
-                <div id="icon-bawah" className=" flex flex-row gap-2 justify-between w-full">
-                  <div id="grup-kiri" className="flex flex-row gap-2">
-                    <div id="1-1" className="flex flex-row gap-2  justify-center items-center  "> 
-                      <h1 className="font-bold text-[#373737] text-2xl">✦</h1> 
-                      <h1 className="text-[12px] font-[900] text-[#373737] text-4xl">Jun 24</h1>
-                    </div>
-                    <div id="1-1" className="flex flex-row gap-2  justify-center items-center  "> 
-                      <h1>🔥</h1> 
-                      <h1 className="text-[12px] font-[900] text-[#373737] text-4xl">10.3k</h1>
-                    </div>
-                    
-                    
-                  </div>
-                  
-                  <div id="grup-kanan" className="flex flex-row">
-                    <div id="1-4" onClick={handleLikeToggle} className="cursor-pointer flex flex-row justify-center items-center "> 
-                      {isLiked ? <IoMdHeart className="text-3xl text-red-600" /> : <FaRegHeart className="text-[#373737] text-3xl" />}
-                      <span className="ms-2 select-none text-[#373737]">10</span>
-                      <h1 className={` text-[#373737] select-none ${isAnimating ? 'animate-spin-grow' : ''}`} >{likeCount}</h1>
-                    </div>
-                  </div>
-                </div>
-            </div>
-
-            <div className="bg-slate-500 flex justify-center">
-                <Img
-                  src="/assets/mwcc2.png"
-                  alt="Shoes"
-                  width={500}
-                  height={700}
-                  className="select-none"
-                />
-                
-            </div>
-            <div>
-              <hr className="bg-black" />
-            </div>
-          </div>
-        </div>
-      
-
-      </div>
-
-      <hr />
-    </div>
-  );
-};
+      <ProjectCard
+      name="Jose Morinho"
+      title="Muhammadiyah Web Content Collaboration"
+      desc="Garbage collection in Python is the process of automatically identifying and reclaiming memory that is no longer in use by the program. This helps to manage memory efficiently and prevent memory leaks."
+      date="Jun 24"
+      views="10.3k"
+      imageSrc="/assets/mwcc2.png"
+      fakeDigit={10}
+      fakeCount={4}
+    />
+      <ProjectCard
+      name="Jose Morinho"
+      title="Muhammadiyah Web Content Collaboration"
+      desc="Garbage collection in Python is the process of automatically identifying and reclaiming memory that is no longer in use by the program. This helps to manage memory efficiently and prevent memory leaks."
+      date="Jun 24"
+      views="10.3k"
+      imageSrc="/assets/mwcc2.png"
+      fakeDigit={1}
+      fakeCount={8}
+    />
+    </>
+  )
+}
 
 export default Projects;
