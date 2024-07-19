d2e184703cdc854972f0c23712d029441cccddbb
MayCleanSitepu <maycleansitepu0204@mail.ugm.ac.id>
2024-07-19 14:18:36 +0700

    playing card layout

--- src/components/Hero/Kartu.jsx
+++ src/components/Hero/Kartu.jsx
@@ -23,7 +23,15 @@ export default function Kartu() {
         className=" lg:h-60 lg:w-[30rem] lg:mr-5 h-40 w-[19rem] ml-auto "
       >
         <SwiperSlide className="border-2 border-dashed border-[#949494]"> 
-          <h1 className="text-[#373737]">20 Tahun</h1> 
+          <div className="flex flex-col justify-between w-full h-full p-5">
+            <div className="text-[#373737]">
+                <h1 className="absolute top-3 left-6 rotate-90 text-[#f95858]">♦</h1>
+            </div>
+            <h1 className="text-[#373737] flex justify-center ">20 Tahun</h1> 
+            <div className="text-[#373737]">
+                <h1 className="absolute bottom-3 right-6 rotate-90 text-[#f95858]">♦</h1>
+            </div>
+          </div>
           </SwiperSlide>
         <SwiperSlide className="border-2 border-dashed border-[#949494]">
           <h1 className="text-[#373737]">Domisili Jogja</h1>  
