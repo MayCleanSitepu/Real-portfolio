5c32687be84b096073dc872b2be06c3f4bd63135
May Clean Sitepu <113108027+MayCleanSitepu@users.noreply.github.com>
2025-02-20 16:04:28 +0700

    Fix: uneeded div punished

--- src/app/project/Showcase/page.js
+++ src/app/project/Showcase/page.js
@@ -62,7 +62,7 @@ function Showcase() {
                 {/* <Scene/> */}
                 <div className="absolute inset-0 -z-10 h-[500vh] w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
                 <Hero/> 
-                <Edu/>  
+                // <Edu/>  
                 <Tech/> 
                 {/* <Maintenance/> */}
                 <Projects/>
